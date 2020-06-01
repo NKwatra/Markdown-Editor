c85a7e75b9ccd37c1abb89506220d0fec9f78cd5
NKwatra <kwatranishkarsh@gmail.com>
2020-06-01 09:39:22 +0530

    add test for complete app component

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('passed', () => {
-  
+test('markdown is updated when user clicks send button', () => {
+    // Arrange
+    const {container, getByTestId, getByText} = render(<App />);
+    const markdownContainer = container.querySelector('.markdown-container');
+    const userInputTextArea = getByTestId('user-input');
+    const sendButton = getByText(/send/i);
+    const expectedHtml = `<p><strong>Lorem ipsum</strong> dolor <em>sit amet</em></p>`;
+
+    // Act 
+    fireEvent.change(userInputTextArea, {target : {value : "**Lorem ipsum** dolor *sit amet*"}});
+    fireEvent.click(sendButton);
+
+    // Assert
+    expect(markdownContainer.innerHTML).toBe(expectedHtml);
+
 })
